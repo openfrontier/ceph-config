11567626db00dd0593caaf5f9948eab2d11349d4
xingjiudong <25635680@qq.com>
2017-08-02 16:39:27 +0800

    Fix bug of default ETCD_CLIENT_IP

--- ceph-config.sh
+++ ceph-config.sh
@@ -4,9 +4,9 @@ set -e
 ETCD_CLIENT_IP=$1
 CLUSTER_NAME=$2
 
-docker run --rm \
+docker run --net=host --rm \
 -v /etc/ceph:/etc/ceph \
--e ETCD_CLIENT_IP=${ETCD_CLIENT_IP} \
+-e ETCD_CLIENT_IP=${ETCD_CLIENT_IP:-127.0.0.1} \
 -e CLUSTER=${CLUSTER_NAME:-ceph} \
 openfrontier/ceph-config
 
