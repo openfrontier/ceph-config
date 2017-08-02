e4e4bf0eec1d970830dbbeeb666025478fafdb04
docker <25635680@qq.com>
2017-08-02 15:58:41 +0800

    delete Default value of ETCD_CLIENT_IP

--- ceph-config.sh
+++ ceph-config.sh
@@ -6,7 +6,7 @@ CLUSTER_NAME=$2
 
 docker run --rm \
 -v /etc/ceph:/etc/ceph \
--e ETCD_CLIENT_IP=${ETCD_CLIENT_IP:-127.0.0.1} \
+-e ETCD_CLIENT_IP=${ETCD_CLIENT_IP} \
 -e CLUSTER=${CLUSTER_NAME:-ceph} \
 openfrontier/ceph-config
 
